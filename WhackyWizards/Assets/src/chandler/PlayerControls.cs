3aab39150234c609ad962da32c23998dfd283cec
Chandler Calkins <chandlerjaycalkins@gmail.com>
2021-09-16 16:20:38 -0700

    Added a moveable test player and test scene
    Added a test scene with an input scheme and a script that makes it so the test player can move

---
+++ WhackyWizards/Assets/src/chandler/PlayerControls.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/src/chandler/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerDefault"",
+            ""id"": ""367c3de2-ebd3-4a9c-8368-63ff62a0582a"",
+            ""actions"": [
+                {
+                    ""name"": ""BasicMovement"",
+                    ""type"": ""Button"",
+                    ""id"": ""b7cb41a3-a531-474b-b923-f1c39d77a496"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""BasicMovement"",
+                    ""id"": ""8903b530-7463-42de-b7f5-2ca24e2ee121"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""BasicMovement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""5b87da94-86ac-4a52-aab0-a02fbedd9386"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""BasicMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8316d142-38ab-4ed3-aa87-f88aef406128"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""BasicMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""6cbc8067-980e-43b6-95aa-141292b9372d"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""BasicMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""b778293b-7420-4862-8a5f-66b7219dae5e"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""BasicMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerDefault
+        m_PlayerDefault = asset.FindActionMap("PlayerDefault", throwIfNotFound: true);
+        m_PlayerDefault_BasicMovement = m_PlayerDefault.FindAction("BasicMovement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerDefault
+    private readonly InputActionMap m_PlayerDefault;
+    private IPlayerDefaultActions m_PlayerDefaultActionsCallbackInterface;
+    private readonly InputAction m_PlayerDefault_BasicMovement;
+    public struct PlayerDefaultActions
+    {
+        private @PlayerControls m_Wrapper;
+        public PlayerDefaultActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @BasicMovement => m_Wrapper.m_PlayerDefault_BasicMovement;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerDefault; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerDefaultActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerDefaultActions instance)
+        {
+            if (m_Wrapper.m_PlayerDefaultActionsCallbackInterface != null)
+            {
+                @BasicMovement.started -= m_Wrapper.m_PlayerDefaultActionsCallbackInterface.OnBasicMovement;
+                @BasicMovement.performed -= m_Wrapper.m_PlayerDefaultActionsCallbackInterface.OnBasicMovement;
+                @BasicMovement.canceled -= m_Wrapper.m_PlayerDefaultActionsCallbackInterface.OnBasicMovement;
+            }
+            m_Wrapper.m_PlayerDefaultActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @BasicMovement.started += instance.OnBasicMovement;
+                @BasicMovement.performed += instance.OnBasicMovement;
+                @BasicMovement.canceled += instance.OnBasicMovement;
+            }
+        }
+    }
+    public PlayerDefaultActions @PlayerDefault => new PlayerDefaultActions(this);
+    public interface IPlayerDefaultActions
+    {
+        void OnBasicMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ WhackyWizards/Assets/src/chandler/PlayerScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    private InputAction movement;
+    private PlayerControls controls;
+
+    public Rigidbody2D rb;
+
+    public float movementspeed;
+    private Vector2 moves;
+
+    void Awake()
+    {
+        this.controls = new PlayerControls();
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        this.moves += movement.ReadValue<Vector2>() * movementspeed;
+        this.rb.AddForce(moves, ForceMode2D.Impulse);
+        this.moves = Vector2.zero;
+    }
+
+    void OnEnable()
+    {
+        this.movement = controls.PlayerDefault.BasicMovement;
+
+        this.movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        this.movement.Disable();
+    }
+}
