d53955454cfb5396edcdf7b5567efd7e63b86465
Chandler Calkins <chandlerjaycalkins@gmail.com>
2021-09-17 01:14:11 -0700

    Player now rotates to follow mouse
    The player sprite now rotates to follow the mouse cursor. I also added some reference squares in the test scene and uncommented the line that makes the main camera follow the player so that can be on display now.

--- WhackyWizards/Assets/src/chandler/MainCamera.cs
+++ WhackyWizards/Assets/src/chandler/MainCamera.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour
 {
     public Transform playerPosition;
+    // mouse position
+    private static Vector3 mousePoint;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,14 @@ public class MainCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // gets the coordinates of the mouse cursor
+        mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         // uncomment this once tilemaps get added so that the camera following the player will be visible
-        // this.transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y, this.transform.position.z);
+    }
+
+    public static Vector3 getMousePoint()
+    {
+        return mousePoint;
     }
 }
--- WhackyWizards/Assets/src/chandler/PlayerScript.cs
+++ WhackyWizards/Assets/src/chandler/PlayerScript.cs
@@ -6,46 +6,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    // links to inputs / controls
     private InputAction movement;
     private PlayerControls controls;
 
+    // player's rigidbody component
     public Rigidbody2D rb;
-
+    // speed of the player
     public float movementspeed;
-    private Vector2 moves;
 
     void Awake()
     {
         this.controls = new PlayerControls();
-        rb = this.GetComponent<Rigidbody2D>();
+        this.rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // gets the position of the mouse and rotates the player towards the mouse
+        Vector3 mousePoint = MainCamera.getMousePoint();
+        Vector3 difference = mousePoint - this.transform.position;
+        float angle = (float) ((180 / Math.PI) * Math.Atan((double)(difference.y / difference.x)));
+        this.transform.eulerAngles = Vector3.forward * angle;
     }
 
     void FixedUpdate()
     {
-        this.moves += movement.ReadValue<Vector2>() * movementspeed;
-        this.rb.AddForce(moves, ForceMode2D.Impulse);
-        this.moves = Vector2.zero;
+        // reads WASD input from the player, multiplies that input by the movement speed, and moves the player that direction
+        this.rb.AddForce(movement.ReadValue<Vector2>() * movementspeed, ForceMode2D.Impulse);
     }
 
+    // handles enabling unity input package scheme
     void OnEnable()
     {
         this.movement = controls.PlayerDefault.BasicMovement;
-
         this.movement.Enable();
     }
 
+    // handles disabling unity input package scheme
     void OnDisable()
     {
         this.movement.Disable();
