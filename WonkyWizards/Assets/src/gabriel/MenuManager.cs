c8a2f0cc8f887d09ee12e53771dcc74eb3ea34a2
GabrielSJones <35454714+GabrielSJones@users.noreply.github.com>
2021-09-20 00:32:12 -0700

    Scene Changer and Mouse Pos resetter with new input sys

--- WonkyWizards/Assets/src/gabriel/MenuManager.cs
+++ WonkyWizards/Assets/src/gabriel/MenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MenuManager : MonoBehaviour
 {
@@ -12,5 +13,18 @@ public class MenuManager : MonoBehaviour
         Debug.Log("Exiting Game\n");
         Application.Quit();
     }
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("PlayerTestLevel");
+    }
 
+    //CALL THIS FUNC WHENEVER SWITCHING SCENES TO AVOID DISORIENTATION
+    public void MousePositionZero()
+    {
+        Vector2 ScreenMiddle = new Vector2();
+        ScreenMiddle.x = Screen.width / 2;
+        ScreenMiddle.y = Screen.height / 2;
+
+        Mouse.current.WarpCursorPosition(ScreenMiddle);
+    }
 }
