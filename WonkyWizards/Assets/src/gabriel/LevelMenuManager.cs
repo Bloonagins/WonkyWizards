7c322ad5d6d67f189dd188fc838891a7dbbff960
GabrielSJones <35454714+GabrielSJones@users.noreply.github.com>
2021-09-29 00:06:56 -0700

    Pause menu functionality complete
    esc key pauses/unpauses, and can also be used to resume the game once inside menus

--- WonkyWizards/Assets/src/gabriel/LevelMenuManager.cs
+++ WonkyWizards/Assets/src/gabriel/LevelMenuManager.cs
@@ -1,17 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LevelMenuManager : MonoBehaviour
 {
+    private ControlScheme controls;
+    private InputAction pause;
+
+    private bool bIsPaused = false;
+
+    //Per level this pause menu object will have to be assigned
+    public GameObject PauseMenuObject;
+    public GameObject ConfirmMenuObject;
+    public GameObject OptionsMenuObject;
 
 
-    private static bool bIsPaused = false;
-    public static GameObject PauseMenuObject;
 
+    private void Awake()
+    {
+        controls = new ControlScheme();
+    }
+    private void OnEnable()
+    {
+        pause = controls.PlayerDefault.Pause;
+        pause.performed += POrResGame;
+        pause.Enable();
+    }
+    private void OnDisable()
+    {
+        pause.performed -= POrResGame;
+        pause.Disable();
+    }
+
+    private void POrResGame(InputAction.CallbackContext obj)
+    {
+        Debug.Log("attempting to pause/unpause game...\n");
+        PauseOrResumeGame();
+    }
 
     //Calling this func will attempt to pause/resume the game
-    public static void PauseOrResumeGame()
+    public void PauseOrResumeGame()
     {
         if(bIsPaused == false)
         {
@@ -22,11 +52,13 @@ public class LevelMenuManager : MonoBehaviour
         else
         {
             PauseMenuObject.SetActive(false);
+            OptionsMenuObject.SetActive(false);
+            ConfirmMenuObject.SetActive(false);
+
             Time.timeScale = 1f;
             bIsPaused = false;
         }
     }
-
 
     //Calling this function will close the application
     public void ExitGame()
@@ -34,5 +66,13 @@ public class LevelMenuManager : MonoBehaviour
         Debug.Log("Exiting Game\n");
         Application.Quit();
     }
+
+
+
+
+
+
+
+
 
 }
