97c807464a95fc507cabf8eb6aa075767e7f0bd7
Ethan <etbcor@gmail.com>
2021-09-28 18:22:10 -0700

    [TESTING] Created script that just spawns a barrier (using Instantiate())

---
+++ WonkyWizards/Assets/src/ethan/SpawnBarrierExample.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBarrierExample : MonoBehaviour
+{
+
+    public GameObject summon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(summon, PlayerScript.worldCursorPoint, Quaternion.identity);
+            Debug.Log(PlayerScript.worldCursorPoint);
+        }
+    }
+}
