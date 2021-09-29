a3941bc4609a2a0d669c226019f4e27129fa9061
nardgag18 <gage.nardi@outlook.com>
2021-09-29 16:34:23 -0700

    Level 1 push
    First tile map for level 1.

---
+++ WonkyWizards/Assets/src/gage/LevelManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Setup faze! Get ready for the enemy wave!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
