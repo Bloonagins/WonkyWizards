1265f6d2c05c48a8fc9a4c7528db42ef38461e30
GabrielSJones <35454714+GabrielSJones@users.noreply.github.com>
2021-09-29 20:48:30 -0700

    Updated script

--- WonkyWizards/Assets/src/gabriel/UIManager.cs
+++ WonkyWizards/Assets/src/gabriel/UIManager.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public Text PlayerMode;
+    public TextMeshPro PlayerMode;
+    public bool CastMode;
+    public bool BuildMode;
 
+    private void Awake()
+    {
+        BuildMode = true;
+        CastMode = false;
+    }
 
-    //PlayerMode.
+    //Calling this funtion will set cast/build bools and set UI text to correspond to those values
+    public void UpdatePlayerModeUI()
+    {
+        if(CastMode)
+        {
+            CastMode = true;
+            BuildMode = false;
+            PlayerMode.text = "Cast Mode";
+            PlayerMode.color = new Color(209, 73, 30, 255);
+            //D1491E
+
+        }
+        else {
+            CastMode = false;
+            BuildMode = true;
+            PlayerMode.text = "Build Mode";
+            PlayerMode.color = new Color(52, 209, 30, 255);
+            //34D11E
+        }
+
+    }
 
 
 
