d4aee3e9e877197011f54ce02b9d17034eae0365
David Bush <bush0386@vandals.uidaho.edu>
2021-10-02 22:07:50 -0700

    Fixed Pathfinding Scene

--- WonkyWizards/Assets/src/david/Scripts/SimpleAiMovement.cs
+++ WonkyWizards/Assets/src/david/Scripts/SimpleAiMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SimpleAiMovement : MonoBehaviour
+public class TestMovement : MonoBehaviour
 {
     public Transform goal;
     // Start is called before the first frame update
