cf6f7026c47f8a323983e8d3ff813c6cf4b4ee71
Ethan <etbcor@gmail.com>
2021-10-04 19:21:33 -0700

    Update Barrier.cs

--- WonkyWizards/Assets/src/ethan/Barrier.cs
+++ WonkyWizards/Assets/src/ethan/Barrier.cs
@@ -6,17 +6,5 @@ public class Barrier : Summon
 {
     public const int cost = 10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public int getCost () { return cost; }
 }
