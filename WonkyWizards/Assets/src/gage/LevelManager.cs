e600cc92268845aa3c9e8d58c4a1c66f4c499558
nardgag18 <gage.nardi@outlook.com>
2021-10-10 15:40:19 -0700

    Updated levelmanager.cs to create the level1 traversable array
    - level1Arr is a public static bool array that is 12 by 12 to represent the level 1 tile-map

--- WonkyWizards/Assets/src/gage/LevelManager.cs
+++ WonkyWizards/Assets/src/gage/LevelManager.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static bool[,] level1Arr = new bool[11, 11]; // static array of level 1
+    int row, col;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Wave 1 is beginning!");
+        //
+        for(row = 0; row < 12; row++)
+        {
+            for (col = 0; col < 12; col++)
+            {
+                // check positions if they are on nontraversable tiles
+                if ((row == 1 && col == 2) || (row == 1 && col == 9) || (row == 4 && col == 2) || (row == 4 && col == 9) || (row == 7 && col == 2) || (row == 7 && col == 9) || (row == 11 && col == 5))
+                {
+                    level1Arr[row, col] = false; // set tile to not traversable
+                }
+                else
+                {
+                    level1Arr[row, col] = true; // set tile to traversable
+                }
+            }
+        }
     }
 
     // Update is called once per frame
