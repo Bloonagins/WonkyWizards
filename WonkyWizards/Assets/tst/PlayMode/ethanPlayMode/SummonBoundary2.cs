872e094db81ff8060e1395d71ee70dba6a5ca6ef
Ethan <etbcor@gmail.com>
2021-10-11 21:50:26 -0700

    First push for Ethan's tests

---
+++ WonkyWizards/Assets/tst/PlayMode/ethanPlayMode/SummonBoundary2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SummonBoundary2 : MonoBehaviour
+{
+   [UnityTest]
+   public IEnumerator SummonBoundaryTest2()
+    {
+
+
+        yield return null;
+    }
+}
---
+++ WonkyWizards/Assets/tst/PlayMode/ethanPlayMode/SummonStress.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SummonStress : MonoBehaviour
+{
+    [UnityTest]
+    public IEnumerator SummonStressTest()
+    {
+
+
+        yield return null;
+    }
+}
