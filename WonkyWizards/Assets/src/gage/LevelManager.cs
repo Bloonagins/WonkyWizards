879e5da7d7c92ad08dc012ce5b8b5e9efbbb6192
nardgag18 <gage.nardi@outlook.com>
2021-10-11 19:53:01 -0700

    LevelManager script was updated so Ethan can go back to coding

--- WonkyWizards/Assets/src/gage/LevelManager.cs
+++ WonkyWizards/Assets/src/gage/LevelManager.cs
@@ -1,52 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // package is used to check the scene name in unity
 
 public class LevelManager : MonoBehaviour
 {
-    public static bool[,] level1Arr = new bool[11, 11]; // static array of level 1
-    int row, col;
-    Scene currentScene; // reference variable to the current scene
-    string sceneName; // reference to the scene's name
+    private static bool[,] level1Arr = new bool[11, 11]; // static array of level 1
+    private static List<bool[,]> levelMasterArray = new List<bool[,]>();
+    
+    private static List<Tuple<int, int>> enemySpawnPoints;
+    private static List<Tuple<int, int>> goals;
+
+    // int array to store the size of each level
+    private static int[] row = {12};
+    private static int[] col = {12};
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelMasterArray.Add(level1Arr);
+        enemySpawnPoints.Add(new Tuple<int, int>(0, 5));
+        goals.Add(new Tuple<int, int>(11, 5));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
 
     void Initialize_Level_1()
     {
         /// Initializes the level 1 bool array of traversable tiles.
-        ///     - true = is traversable
+        ///     - true  = is traversable
         ///     - false = not traversable
-        for (row = 0; row < 12; row++)
+        for (int i = 0; i < row[0]; i++)
         {
-            for (col = 0; col < 12; col++)
+            for (int j = 0; j < col[0]; j++)
             {
-                // check positions if they are on nontraversable tiles
-                if ((row == 1 && col == 2) || (row == 1 && col == 9) || (row == 4 && col == 2) || (row == 4 && col == 9) || (row == 7 && col == 2) || (row == 7 && col == 9) || (row == 11 && col == 5))
+                // set positions certain to non-traversable tiles
+                if ((i == 1 && j == 2) || (i == 1 && j == 9) || 
+                    (i == 4 && j == 2) || (i == 4 && j == 9) || 
+                    (i == 7 && j == 2) || (i == 7 && j == 9) || 
+                    (i == 11 && j == 5))
                 {
-                    level1Arr[row, col] = false; // set tile to not traversable
+                    level1Arr[i, j] = false; // set tile to not traversable
                 }
                 else
                 {
-                    level1Arr[row, col] = true; // set tile to traversable
+                    level1Arr[i, j] = true; // set tile to traversable
                 }
             }
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentScene = SceneManager.GetActiveScene(); // get the current loaded scene
-        sceneName = currentScene.name; // get the current scene name
-        
-        if(sceneName == "Level_1")
-        {
-            // initialize level 1
-            Initialize_Level_1();
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
+    /// <returns> current static array for the level </returns>
+    public static bool [,] getLevelArray()
     {
-        
+        return levelMasterArray.ToArray()[GameManager.getCurrentLevel()];
+    }
+    /// <returns> get the current level's row size </returns>
+    public static int getLevelRows()
+    {
+        return row[GameManager.getCurrentLevel()];
+    }
+    public static int getLevelCols()
+    {
+        return col[GameManager.getCurrentLevel()];
+    }
+    public static Tuple<int, int> getEnemySpawnPoint()
+    {
+        return enemySpawnPoints[GameManager.getCurrentLevel()];
+    }
+    public static Tuple<int, int> getLevelGoal()
+    {
+        return goals[GameManager.getCurrentLevel()];
     }
 }
