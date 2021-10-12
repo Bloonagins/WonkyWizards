5d314af305157ef51ed5dc5e88dadf3e1f6b7d98
nardgag18 <gage.nardi@outlook.com>
2021-10-11 22:29:52 -0700

    Wavespawner script spawns the correct things!
    - Spawns 3 waves
    - 3 enemies in wave 1
    - 5 enemies in wave 2
    - 10 enemies in wave 3 (final wave)

--- WonkyWizards/Assets/src/gage/WaveSpawner.cs
+++ WonkyWizards/Assets/src/gage/WaveSpawner.cs
@@ -17,23 +17,45 @@ public class WaveSpawner : MonoBehaviour
     private Wave currentWave;
     private int currentWaveNumber;
     private bool canSpawn = true;
-    private float nextSpawnTime;
+    private float nextSpawnTime, currentSpawnTime;
+    //private float timer;
+
+    private void Start()
+    {
+        currentSpawnTime = 0.0f; // timer to track when the next enemy spawns
+        nextSpawnTime = 3.0f; // countdown in seconds till next enemy spawn
+    }
 
     private void FixedUpdate()
     {
         currentWave = waves[currentWaveNumber];
-        SpawnWave();
+
+        if (currentSpawnTime > 0)
+        {
+            currentSpawnTime -= Time.fixedDeltaTime; // decrement the timer
+        }
+        else
+        {
+            SpawnWave();
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0 && !canSpawn)
+            {
+                currentWaveNumber++;
+                canSpawn = true;
+            }
+            currentSpawnTime += nextSpawnTime; // increment the time
+        } 
     }
 
     void SpawnWave()
     {
-        if(canSpawn && nextSpawnTime < Time.time) 
+        if(canSpawn) 
         {
             //GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
             //Transform randomPoint = spawnPoints[randomEnemy.Range(0, spawnPoints.Length)];
             Instantiate(currentWave.typeOfEnemies[0], spawnPoints[0].position, Quaternion.identity);
+            nextSpawnTime = 3.0f;
             currentWave.numberOfEnemies--;
-            nextSpawnTime = Time.time + currentWave.spawnInterval;
+            //nextSpawnTime -= Time.fixedDeltaTime;
             if(currentWave.numberOfEnemies == 0)
             {
                 canSpawn = false;
