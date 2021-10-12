811bb021d095b6edbe35b8c8b8a2806ad4eecfa2
nardgag18 <gage.nardi@outlook.com>
2021-10-11 20:41:13 -0700

    did the bake
    - Wavespawner spawns first wave!!!

--- WonkyWizards/Assets/src/gage/WaveSpawner.cs
+++ WonkyWizards/Assets/src/gage/WaveSpawner.cs
@@ -40,35 +40,6 @@ public class WaveSpawner : MonoBehaviour
             }
         }
     }
-
-    /*public GameObject[] spawnPoints;
-    GameObject currentPoint;
-    int index;
-    
-    public Transform enemyPrefab; // assign enemy prefabs
-    public GameObject[] enemies; // array of enemy types
-    public float timeBetweenSpawns; // countdown of time it takes for a enemy to spawn
-    public float timeBetweenWaves = 10f; // 
-
-    public bool canSpawn;
-    
-    private float countdown = 2f;
-
-    void FixedUpdate()
-    {
-        if(countdown <= 0f)
-        {
-            SpawnWave();
-            countdown = timeBetweenWaves;
-        }
-
-        countdown -= Time.deltaTime;
-    }
-    void SpawnWave()
-    {
-        Debug.Log("Wave incoming!");
-    }
-    */
 }
 
 /// Use this to comment functions
