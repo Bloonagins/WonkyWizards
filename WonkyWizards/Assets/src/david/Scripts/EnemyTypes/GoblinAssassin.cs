2215782dc2503e9325024b0da39e9d38bbf8a485
David Bush <bush0386@vandals.uidaho.edu>
2021-10-23 17:35:37 -0700

    Updated GoblinWarrior and Enemy interactions

--- WonkyWizards/Assets/src/david/Scripts/EnemyTypes/GoblinAssassin.cs
+++ WonkyWizards/Assets/src/david/Scripts/EnemyTypes/GoblinAssassin.cs
@@ -1,30 +1,46 @@
 /**********************************************
-| GoblinBerserker V1.0.0                      |
+| GoblinAssasin V1.0.0                        |
 | Author: David Bush, T5                      |
-| Description: This is the GoblinGiant class  |
-| that inherits from the Enemy superclass.    |
+| Description: This is the GoblinAssasin      |
+| class that inherits from the Enemy          |
+| superclass. This will contain all variables |
+| and methods associtiated with the           |
+| GoblinAssasin enemy type. Each GoblinAssasin| 
+| has the unique dash ability. When it is     | 
+| within a certain range from the player the  |
+| Assasain dashes forward towards the player. |
 | Bugs:                                       |
 **********************************************/
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GoblinGiant : Enemy
+public class GoblinAssassin : Enemy
 {
     // Used to store RigidBody2d Component
     private Rigidbody2D rb;
+    // The distance the GoblinAssasin needs to be in order to dash
+    private float dashDistance;
+    // The dash amount applied to the unit
+    private float dashAmount;
+    // The dash cool down
+    private float dashCD;
+    // The dash cool down timer
+    private float dashTimer;
 
-
-    // Constructor for GoblinGiant
-    public GoblinGiant()
+    // Constructor for GoblinAssasin
+    public GoblinAssassin()
     {
-        max_health = health = 400;
-        damage = 50;
-        move_speed = 14f;
-        attack_speed = attackTimer = 1.75f; // 1714 damage per minute
+        max_health = health = 350;
+        damage = 70;
+        move_speed = 20f;
+        attack_speed = attackTimer = 1.25f; // 3,360 damage per minute
+        dashDistance = 8f;
+        dashAmount = 130f;
+        dashCD = dashTimer = 3f;
         targetDistance = 25f;
         attackConnected = false;
-        knock_back = 400f;
+        knock_back = 200f;
     }
     
     // Start is called before the first frame update
@@ -37,11 +53,13 @@ public class GoblinGiant : Enemy
     // Increments the timers if they're on a cooldown
     void FixedUpdate()
     {
-        if (attackTimer <= attack_speed) {
+        // Check timers, and increment time
+        if (attackTimer <= attack_speed) { 
             attackTimer += Time.fixedDeltaTime;
         }
-        //Debug.Log("Current Damage: "+GetDamage());
-        //Debug.Log("Current speed: "+agent.acceleration);
+        if (dashTimer <= dashCD) {
+            dashTimer += Time.fixedDeltaTime;
+        }
     }
     // Update is called once per frame
     void Update()
@@ -52,7 +70,7 @@ public class GoblinGiant : Enemy
             Destroy(gameObject); // Destroy unit
         }
     }
-
+    
     // Function that checks for collisions
     void OnTriggerEnter2D(Collider2D collision)
     {        
@@ -62,31 +80,26 @@ public class GoblinGiant : Enemy
 
             if(other.GetComponent<FireBall>()) { // Check if spell was Fireball
                 RecieveDamage(other.GetComponent<FireBall>().getSpellDamage()); // Recieve damage 
-                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<FireBall>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // Apply Knockback;
+                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<FireBall>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // FireBall.getKnockback();
             }
             else if(other.GetComponent<MagicMissle>()) { // Check if spell was MagicMissle
                 //Debug.Log("Collided Magic Missle");
                 RecieveDamage(other.GetComponent<MagicMissle>().getSpellDamage()); // Recieve damage 
-                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<MagicMissle>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // Apply Knockback;
+                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<MagicMissle>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // FireBall.getKnockback();
             }
             else if(other.GetComponent<IceBeam>()) { // Check if spell was IceBeam
                 //Debug.Log("Collided Ice Beam");
                 RecieveDamage(other.GetComponent<IceBeam>().getSpellDamage()); // Recieve damage 
-                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<IceBeam>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // Apply Knockback;
+                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<IceBeam>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // FireBall.getKnockback();
             }
             else if(other.GetComponent<AcidSpray>()) { // Check if spell was AcidSpray
                 //Debug.Log("Collided Acid Spray");
                 RecieveDamage(other.GetComponent<AcidSpray>().getSpellDamage()); // Recieve damage 
-                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<AcidSpray>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // Apply Knockback;
+                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<AcidSpray>().getSpellKnockBack() * -1.0f, ForceMode2D.Impulse); // FireBall.getKnockback();
             }
+
         }
-        else if(other.tag == "SummonProjectile") {
-            if(other.GetComponent<Sword>()) {
-                Debug.Log("COLLIDED WITH SWORD");
-                RecieveDamage(other.GetComponent<Sword>().getProjDamage()); // Recieve damage
-                rb.AddForce((other.transform.position - transform.position) * other.GetComponent<Sword>().getProjKnockback() * -1.0f, ForceMode2D.Impulse); // Apply Knockback;
-            }
-        }
+
     }
     void OnTriggerStay2D(Collider2D collision)
     {
@@ -101,6 +114,16 @@ public class GoblinGiant : Enemy
         }
     }
 
+
+    //
+    public void ApplyDash(Vector3 player_position) {
+        dashTimer = 0f;
+        rb.AddForce((player_position - transform.position) * dashAmount , ForceMode2D.Impulse);
+    }
+    //
+    public bool canDash() {
+        return dashTimer >= dashCD;
+    }
     // Keeps track of when enemy can attack
     public bool canAttack()
     {
@@ -142,11 +165,11 @@ public class GoblinGiant : Enemy
     }
 
     // Methods for retrieving stats
-    public override int GetMaxHealth()
+    public int GetMaxHealth()
     {
         return max_health;
     }
-    public override int GetHealth()
+    public int GetHealth()
     {
         return health;
     }
@@ -169,6 +192,13 @@ public class GoblinGiant : Enemy
     public float GetAttackTimer()
     {
         return attackTimer;
+    }
+    public float GetDashTimer()
+    {
+        return dashTimer;
+    }
+    public float GetDashDistance() {
+        return dashDistance;
     }
     public float GetTargetDistance() 
     {
