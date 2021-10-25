cc8fdcb5f8cabc0bbddc44bdcbb6ed87a506b4ae
Chandler Calkins <chandlerjaycalkins@gmail.com>
2021-10-24 17:26:04 -0700

    created a demo controller script for the player

---
+++ WonkyWizards/Assets/src/chandler/DemoController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        
+    }
+}
