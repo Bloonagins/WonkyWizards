5c053b549379090eae24acb1ee4315849ae1f261
Chandler Calkins <chandlerjaycalkins@gmail.com>
2021-10-26 14:20:13 -0700

    Added a bit more to the demo controller for the player in level 1
    will remove this stuff after david's presentation

--- WonkyWizards/Assets/src/chandler/DemoController.cs
+++ WonkyWizards/Assets/src/chandler/DemoController.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class DemoController : MonoBehaviour
 {
-    private void FixedUpdate()
+    private static DemoMode dm;
+    private static EnemyRadar er;
+    public GameObject fireball;
+
+    void Start()
     {
-        
+        dm = GetComponent<DemoMode>();
+        er = GetComponent<EnemyRadar>();
+    }
+
+    void Update()
+    {
+        if (dm.GetDemoActive())
+        {
+            if (er.GetInRange())
+            {
+                //cast fireball in direction of closest enemy
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (dm.GetDemoActive())
+        {
+            /*if (enemy nearby)
+            {
+                move away from enemy
+            }
+            else
+            {
+                move towards goal point
+            }*/
+        }
     }
 }
