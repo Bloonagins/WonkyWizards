afa4bea55c95521600f56dabbd0251c9c4edac3d
nardgag18 <gage.nardi@outlook.com>
2021-11-03 17:04:06 -0700

    Fixed levelmanager to help ethan
    Cleaner layout now too

--- WonkyWizards/Assets/src/gage/LevelManager.cs
+++ WonkyWizards/Assets/src/gage/LevelManager.cs
@@ -6,38 +6,26 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     private static bool[,] level1Arr = new bool[12, 12]; // static array of level 1
+    private static bool[,] level2Arr = new bool[12, 12]; // static array of level 2
     private static List<bool[,]> levelMasterArray = new List<bool[,]>();
     
-    private static List<Tuple<int, int>> enemySpawnPoints = new List<Tuple<int,int>>(); // list of enemy spawn points (x, y)
-    private static List<Tuple<int, int>> goals = new List<Tuple<int,int>>();  // list of the 
+    private static Tuple<int, int> spawnPoint; // list of enemy spawn points (x, y)
+    private static Tuple<int, int> goal;  // list of the 
 
     // int array to store the size of each level
-    private static int[] row = {12};
-    private static int[] col = {12};
+    private static int rows;
+    private static int cols;
     private int currentLevel;
   
     // Start is called before the first frame update
     void Start()
     {
-        currentLevel = GameManager.getCurrentLevel(); // gets the current level from zach's function
-        //Debug.Log("Current level: " + currentLevel);
-        levelMasterArray.Add(level1Arr);
-        enemySpawnPoints.Add(new Tuple<int, int>(0, 5));
-        goals.Add(new Tuple<int, int>(11, 5));
-        GameManager.setLevelArray(levelMasterArray[currentLevel]);
+        currentLevel = GameManager.getCurrentLevel(); // gets the current level from zach's GameManager script
         Initiate_Level(currentLevel);
+        levelMasterArray.Add(level1Arr);
+        levelMasterArray.Add(level2Arr);
         
-        /*
-        switch(currentLevel)
-        {
-            case 0:
-                Initialize_Level_1();
-                break;
-            default:
-                Debug.Log("Current level invalide while initializing array");
-                break;
-        } 
-        */
+        GameManager.setLevelArray(levelMasterArray[currentLevel]);
     }
 
     void Initiate_Level(int currentLevel)
@@ -45,7 +33,20 @@ public class LevelManager : MonoBehaviour
         switch (currentLevel)
         {
             case 0:
+                rows = 12;
+                cols = 12;
                 Initialize_Level_1();
+                spawnPoint = new Tuple<int, int>(0, 5);
+                goal = new Tuple<int, int>(11, 5);
+
+                break;
+            case 1:
+                rows = 12;
+                cols = 12;
+                Initialize_Level_2();
+                spawnPoint = new Tuple<int, int>(0, 5);
+                goal = new Tuple<int, int>(11, 5);
+
                 break;
             default:
                 Debug.Log("Current level invalide while initializing array");
@@ -57,9 +58,9 @@ public class LevelManager : MonoBehaviour
         /// Initializes the level 1 bool array of traversable tiles.
         ///     - true  = is traversable
         ///     - false = not traversable
-        for (int i = 0; i < row[0]; i++)
+        for (int i = 0; i < rows; i++)
         {
-            for (int j = 0; j < col[0]; j++)
+            for (int j = 0; j < cols; j++)
             {
                 // set positions certain to non-traversable tiles
                 if ((i == 1 && j == 2) || (i == 1 && j == 9) || 
@@ -77,6 +78,31 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    void Initialize_Level_2()
+    {
+        /// Initializes the level 2 bool array of traversable tiles.
+        ///     - true  = is traversable
+        ///     - false = not traversable
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                // set positions certain to non-traversable tiles
+                if ((i == 1 && j == 2) || (i == 1 && j == 9) ||
+                    (i == 4 && j == 2) || (i == 4 && j == 9) ||
+                    (i == 7 && j == 2) || (i == 7 && j == 9) ||
+                    (i == 11 && j == 5))
+                {
+                    level2Arr[i, j] = false; // set tile to not traversable
+                }
+                else
+                {
+                    level2Arr[i, j] = true; // set tile to traversable
+                }
+            }
+        }
+    }
+
     /// <returns> current static array for the level </returns>
     public static bool [,] getLevelArray()
     {
@@ -85,21 +111,21 @@ public class LevelManager : MonoBehaviour
     /// <returns> get the current level's row size </returns>
     public static int getLevelRows()
     {
-        return row[GameManager.getCurrentLevel()];
+        return rows;
     }
     /// <returns> get the current level's column size </returns>
     public static int getLevelCols()
     {
-        return col[GameManager.getCurrentLevel()];
+        return cols;
     }
     /// <returns> gets the spawn point coordinates for the current level</returns>
     public static Tuple<int, int> getEnemySpawnPoint()
     {
-        return enemySpawnPoints[GameManager.getCurrentLevel()];
+        return spawnPoint;
     }
     /// <returns> gets the enemy's goal for the current level </returns>
     public static Tuple<int, int> getLevelGoal()
     {
-        return goals[GameManager.getCurrentLevel()];
+        return goal;
     }
 }
