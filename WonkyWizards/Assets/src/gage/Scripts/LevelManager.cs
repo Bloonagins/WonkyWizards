c3c7f7e470d1b2aef5daef610431a40d938dca72
nardgag18 <gage.nardi@outlook.com>
2021-12-08 22:49:05 -0800

    Wave adjustments to levels

--- WonkyWizards/Assets/src/gage/Scripts/LevelManager.cs
+++ WonkyWizards/Assets/src/gage/Scripts/LevelManager.cs
@@ -16,6 +16,7 @@ public class LevelManager : MonoBehaviour
     private GameManager gm; // instance of the GameManager
     private static bool[,] level1Arr = new bool[12, 12]; // static array of level 1
     private static bool[,] level2Arr = new bool[12, 12]; // static array of level 2
+    private static bool[,] level3Arr = new bool[12, 12]; // static array of level 3
     private static bool[,] currentLevelArr; // array variable to point to the current level
     
     private static Tuple<int, int> spawnPoint; // list of enemy spawn points (x, y)
@@ -58,6 +59,15 @@ public class LevelManager : MonoBehaviour
                 goal = new Tuple<int, int>(11, 5);
 
                 break;
+            case 2: // second level
+                rows = 12;
+                cols = 12;
+                Initialize_Level_3();
+                currentLevelArr = level2Arr;
+                spawnPoint = new Tuple<int, int>(0, 5);
+                goal = new Tuple<int, int>(11, 5);
+
+                break;
             default:
                 Debug.Log("Current level invalide while initializing array [LevelManager.cs]");
                 break;
@@ -85,6 +95,16 @@ public class LevelManager : MonoBehaviour
                 level2Arr[i, j] = true; 
     }
 
+    void Initialize_Level_3()
+    {
+        /// Initializes the level 3 bool array of traversable tiles.
+        ///     - true  = is traversable
+        ///     - false = not traversable
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                // set tile to traversable
+                level3Arr[i, j] = true;
+    }
     /// <returns> current static array for the level </returns>
     public static bool [,] getLevelArray()
     {
--- WonkyWizards/Assets/src/gage/Scripts/WaveSpawner.cs
+++ WonkyWizards/Assets/src/gage/Scripts/WaveSpawner.cs
@@ -39,7 +39,8 @@ public class WaveSpawner : MonoBehaviour
     private bool baked; // variable to check if nav mesh has been baked before each wave starts
     private bool givenMana;
     private bool spawnedBoss = false;
-    
+   // private int currentLevel;
+    //private GameManager gm; // instance of the GameManager
     public NavMeshSurface2d Surface2D; // navmesh component variable
 
     // --- Start of Singleton Pattern --- \\
@@ -77,6 +78,8 @@ public class WaveSpawner : MonoBehaviour
 
     private void Start()
     {
+        //gm = GameManager.getSingleton(); // get the current instance of the singleton from the GameManager script
+        //currentLevel = gm.getCurrentLevel(); // gets the current level from zach's GameManager script
         currentSpawnTime = 0.0f; // timer to track when the next enemy spawns
         //mainTimer = 0.0f;
         nextSpawnTime = 3.0f; // countdown in seconds till next enemy spawn
@@ -116,9 +119,16 @@ public class WaveSpawner : MonoBehaviour
                 // check if the last enemy of the wave has been killed
                 if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0 && !canSpawn)
                 {
-                    if (1 < currentWaveNumber) // If completed the last wave
+                    if (1 < currentWaveNumber || waves.Length == currentWaveNumber) // If completed the last wave
                     {
-                        GameManager.ChangeState(GameState.WIN); // set gamestate to win!
+                        //if (currentLevel == 2)
+                        //{
+                            //GameManager.ChangeState(GameState.WIN_SWITCH);
+                        //}
+                        //else
+                        //{
+                          GameManager.ChangeState(GameState.WIN); // set gamestate to win!
+                        //}
                     }
                     else
                     {
